503b4309667c18669ed118deb14612ede6c08ede
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2019-11-03 04:19:18 +0100

    update tutorial utils

--- rnapoliii/modeling/tutorial_util.py
+++ rnapoliii/modeling/tutorial_util.py
@@ -39,6 +39,46 @@ def _align(self):
                 continue
         self.model.update()
 
+# Monkey patch AnalysisReplicaExchange.__init__ to incorporate a post-2.11
+# bug fix
+def _init(self,model,
+                 stat_files,
+                 best_models=None,
+                 score_key=None,
+                 alignment=True):
+    """
+    Construction of the Class.
+    @param model IMP.Model()
+    @param stat_files list of string. Can be ascii stat files, rmf files names
+    @param best_models Integer. Number of best scoring models, if None: all models will be read
+    @param alignment boolean (Default=True). Align before computing the rmsd.
+    """
+    self.model=model
+    self.best_models=best_models
+    self.stath0=IMP.pmi.output.StatHierarchyHandler(model,stat_files,self.best_models,score_key,cache=True)
+    self.stath1=IMP.pmi.output.StatHierarchyHandler(StatHierarchyHandler=self.stath0)
+
+    self.rbs1, self.beads1 = IMP.pmi.tools.get_rbs_and_beads(IMP.pmi.tools.select_at_all_resolutions(self.stath1))
+    self.rbs0, self.beads0 = IMP.pmi.tools.get_rbs_and_beads(IMP.pmi.tools.select_at_all_resolutions(self.stath0))
+    self.sel0_rmsd=IMP.atom.Selection(self.stath0)
+    self.sel1_rmsd=IMP.atom.Selection(self.stath1)
+    self.sel0_alignment=IMP.atom.Selection(self.stath0)
+    self.sel1_alignment=IMP.atom.Selection(self.stath1)
+    self.clusters=[]
+    # fill the cluster list with a single cluster containing all models
+    c = IMP.pmi.output.Cluster(0)
+    self.clusters.append(c)
+    for n0,d0 in enumerate(self.stath0):
+        c.add_member(n0,d0)
+    self.pairwise_rmsd={}
+    self.pairwise_molecular_assignment={}
+    self.alignment=alignment
+    self.symmetric_molecules={}
+    self.issymmetricsel={}
+    self.update_seldicts()
+    self.molcopydict0=IMP.pmi.tools.get_molecules_dictionary_by_copy(IMP.atom.get_leaves(self.stath0))
+    self.molcopydict1=IMP.pmi.tools.get_molecules_dictionary_by_copy(IMP.atom.get_leaves(self.stath1))
+
 
 # This method isn't in IMP 2.11, so patch it in
 def _classify_crosslinks_by_score(self, number_of_classes):
@@ -88,6 +128,7 @@ def _setup_srb(self,hiers,max_trans,max_rot,axis):
 
 
 if IMP.__version__ == '2.11.1':
+    IMP.pmi.macros.AnalysisReplicaExchange.__init__ = _init
     IMP.pmi.macros.AnalysisReplicaExchange.align = _align
     IMP.pmi.io.crosslink.CrossLinkDataBase.append_database = _append_database
     IMP.pmi.io.crosslink.CrossLinkDataBase.classify_crosslinks_by_score \
