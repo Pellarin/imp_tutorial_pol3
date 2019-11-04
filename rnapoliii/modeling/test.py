e37d5e362593193f6f1a2dd0329b6a5f1ad62a8d
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2019-11-04 13:52:29 +0100

    consolidate test.py and test_analysis.py

--- rnapoliii/modeling/test.py
+++ rnapoliii/modeling/test.py
@@ -12,6 +12,7 @@ import IMP.rmf
 import IMP.pmi.macros
 import IMP.pmi.topology
 import tutorial_util
+import pylab
 
 import sys
 
@@ -219,14 +220,14 @@ outputobjects.append(gem)
 # ```mc1.execute_macro()```
 # 
 
-# In[ ]:
+quick=False
+if not quick:
 
+    # total number of saved frames
+    num_frames = 2
 
-# total number of saved frames
-num_frames = 2
-
-# This object defines all components to be sampled as well as the sampling protocol
-mc1=IMP.pmi.macros.ReplicaExchange0(m,
+    # This object defines all components to be sampled as well as the sampling protocol
+    mc1=IMP.pmi.macros.ReplicaExchange0(m,
               root_hier=root_hier,                         # The root hierarchy
               monte_carlo_sample_objects=dof.get_movers()+xl1rest.get_movers(), # All moving particles and parameters
               rmf_output_objects=outputobjects,            # Objects to put into the rmf file
@@ -241,7 +242,168 @@ mc1=IMP.pmi.macros.ReplicaExchange0(m,
               save_coordinates_mode="25th_score",
               global_output_directory=output_directory)
 
-# Start Sampling
-mc1.execute_macro()
+    # Start Sampling
+    mc1.execute_macro()
+
+
+nmodels=10
+rmffile="./results/150_xl_cryoem_3.rmf"
+
+import IMP.pmi.output
+
+hh=IMP.pmi.output.StatHierarchyHandler(m,rmffile)
+
+#Total number of frames
+print("Frames",len(hh))
+
+# Describe the content of the first frame of the rmf file
+print(hh[0])
+
+#list down all the features names
+for k in hh[0].features.keys(): print(k)
+    
+
+# For instance we can compute the distance between two residues
+
+
+p0=IMP.atom.Selection(hh,molecule="C31",residue_index=10).get_selected_particles()[0]
+p1=IMP.atom.Selection(hh,molecule="C34",residue_index=10).get_selected_particles()[0]
+
+d0=IMP.core.XYZ(p0)
+d1=IMP.core.XYZ(p1)
+
+#note that hh can be used as a list
+#pylab.plot([IMP.core.get_distance(d0,d1) for h in hh]);
+
+#pylab.figure()
+
+# Or we can get the radius of gyration of the whole complex
+ps=IMP.atom.Selection(hh).get_selected_particles()
+#pylab.plot([IMP.atom.get_radius_of_gyration(ps) for h in hh])
+
+# To reduce I/O, we can store the data structure internal to hh, 
+# so that it is not read directly from the files
+# and it is faster
+
+data=hh.data
+
+# Then we plot the scores
+#pylab.plot([x.score for x in data])
+
+#pylab.figure() 
+
+# finally we plot the distance of two crosslinked residues
+#pylab.plot([float(x.features["CrossLinkingMassSpectrometryRestraint_Score_|XL|29.APO.1|C31|91|C160|1458|0|CLASS_0|"]) for x in data]);
+
+
+scores={}
+
+for xl in xl1: 
+    if not xl['IntraRigidBody']:
+        scores.update({xl['XLUniqueSubID']:float(xl['IDScore'])})
+    
+
+x=[]
+y=[]
+for k in data[0].features.keys():
+    if "Distance" in k:
+        id=k.split("|")[2]
+        if id in scores:
+            x.append(scores[id])
+            y.append(float(data[2].features[k]))
+
+#pylab.scatter(x,y);
+
+
+are=IMP.pmi.macros.AnalysisReplicaExchange(m,
+                 [rmffile],
+                 best_models=nmodels,
+                 alignment=False)
+
+print(are)
+
+
+are.set_rmsd_selection(molecules=["C31","C34","C53","C37","C82"])
+
+are.cluster(10.0)
+
+# see the contant of the "are" object
+print(are)
+
+#print the cluster info
+for cluster in are:
+    print(cluster)
+
+for member in are[0]:
+    print(member)
+    
+are.save_coordinates(are[0])
+
+# slow!
+# are.plot_rmsd_matrix("rmsd_matrix.pdf")
+
+
+keys=[k for k in are[0][0].features.keys() if "CrossLinkingMassSpectrometryRestraint" in k]
+#pylab.figure()
+for ind in ["0","1","2"]:
+    distances={}
+    psi=[]
+    sigma=[]
+    for member in are[0]:
+        for k in keys:
+            if "CLASS_"+str(ind) in k and "Distance" in k:
+                if k in distances:
+                    distances[k].append(float(member.features[k]))
+                else:
+                    distances[k]=[float(member.features[k])]
+            if "CLASS_"+str(ind) in k and "Psi" in k and "MonteCarlo" not in k:
+                psi.append(float(member.features[k]))
+            if "SIGMA" in k and "Sigma" in k and "MonteCarlo" not in k:
+                sigma.append(float(member.features[k]))
+
+    x=[distances[k] for k in distances]
+    #pylab.boxplot(x);
+    #pylab.figure()
+    #pylab.plot(range(len(psi)),psi);
+    #pylab.figure()
+#pylab.plot(range(len(sigma)),sigma);
+
+
+
+
+### build the seed 
+
+are.write_seed("seed.rmf3", 2)
+
+are.compute_cluster_center(cluster=are[0])
+print(are.precision(cluster=are[0]))
+print(are.bipartite_precision(cluster1=are[0],cluster2=are[1]))
+
+rmsf1=are.rmsf(cluster=are[0],molecule='C31');
+#pylab.plot(list(rmsf1.keys()),list(rmsf1.values()),marker=".",linewidth=0)
+#pylab.figure()
+
+rmsf2=are.rmsf(cluster=are[0],molecule='C34');
+#pylab.plot(list(rmsf2.keys()),list(rmsf2.values()),marker=".",linewidth=0)
+
+for mol in ['ABC23','ABC10beta','ABC14_5','ABC27','C25','AC40','C160','ABC10alpha','C128','AC19','C11','C17','C31','C34','C53','C37','C82']: 
+    are.rmsf(cluster=are[0],molecule=mol);
+ch1=IMP.pmi.tools.ColorHierarchy(are.stath1)
+ch1.color_by_uncertainty()
+are.save_coordinates(are[0])
+
+density_names={'core': ['ABC23','ABC10beta','ABC14_5','ABC27','C25','AC40','C160','ABC10alpha','C128','AC19','C11','C17'],
+               'C53': ['C53'], 
+               'C37': ['C37'], 
+               'C34': ['C34'], 
+               'C82': ['C82'], 
+               'C31': ['C31']}
+
+# you can iterate on the clusters
+for n,a in enumerate(are):
+    are.save_densities(cluster=a,density_custom_ranges=density_names,prefix="Cluster-"+str(n))
+
+# it is slow
+# are.contact_map(cluster=are[0]);
 
 
